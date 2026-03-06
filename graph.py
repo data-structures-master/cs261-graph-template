76ba996034459b696813a53159c495093a0d6161
Jill Hubbard <jill.m.hubbard@gmail.com>
2026-03-05 22:12:03 -0800

    Refactor comments and class names

--- graph.py
+++ graph.py
@@ -1,12 +1,13 @@
 # Graph: An efficient graph.
-# A graph implementation that uses an adjacency list to represent vertices and edges.
+# A graph implementation that uses an adjacency list to represent vertices
+# and edges.
 # Your implementation should pass the tests in test_graph.py.
 
-# YOUR NAME
-# YOUR COLLABORATORS
-# TIME SPENT: 
+# Name:
+# Collaborators:
+# Time spent:
 
-import functools
 
-class FixMe:
-    pass  
+class WhatAmI_Its_Existential_Crisis_Time:
+    pass
+    
